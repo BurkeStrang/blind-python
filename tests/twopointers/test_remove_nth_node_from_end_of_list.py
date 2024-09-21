dc0f8275469b06e0c4f99b4da5f703f2a3edec66
BurkeStrang <burke.strang@outlook.com>
2024-09-21 16:07:45 -0400

    update test

--- tests/twopointers/test_remove_nth_node_from_end_of_list.py
+++ tests/twopointers/test_remove_nth_node_from_end_of_list.py
@@ -1,18 +1,12 @@
-
 import unittest
-import sys
-
-sys.path.append('/home/bstrang/repos/blind-python/problems/twopointers/')
-sys.path.append('/home/bstrang/repos/blind-python/problems/shared/')
-
-import linked_list_node
-import remove_nth_node_from_end_of_list
+from problems.shared.linked_list_node import LinkedListNode
+from problems.twopointers.remove_nth_last_node import remove_nth_last_node
 
 
 class TestRemoveNthNodeFromEndOfList(unittest.TestCase):
     def test_example1(self):
-        input_linked_list = linked_list_node.LinkedListNode(
-            1, linked_list_node.LinkedListNode(2))
-        expected_output = linked_list_node.LinkedListNode(1)
-        self.assertEqual(remove_nth_node_from_end_of_list.remove_nth_last_node
-                         (input_linked_list, 1).data, expected_output.data)
+        input_linked_list = LinkedListNode(
+            1, LinkedListNode(2))
+        expected_output = LinkedListNode(1)
+        self.assertTrue(remove_nth_last_node(
+            input_linked_list, 1).__eq__(expected_output))
--- tests/twopointers/test_valid_palindrome.py
+++ tests/twopointers/test_valid_palindrome.py
@@ -1,11 +1,7 @@
 import unittest
-import sys
-
-sys.path.append('/home/bstrang/repos/blind-python/problems/twopointers/')
-
-import valid_palindrome
+from problems.twopointers.valid_palindrome import is_palindrome
 
 
 class TestValidPalindrome(unittest.TestCase):
     def test_example1(self):
-        self.assertTrue(valid_palindrome.is_palindrome('racecar'))
+        self.assertTrue(is_palindrome('racecar'))
